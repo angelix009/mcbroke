c3c00db26d96c8771aef13f15ef9da8acb305679
benjelloun <othmane.benjelloun@epita.fr>
2025-03-01 17:00:27 +0100

    www

--- index.php
+++ index.php
@@ -1,24 +1,3 @@
 <?php
-// Afficher toutes les erreurs pour le débogage
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-echo "<h1>Contenu du répertoire</h1>";
-echo "<pre>";
-system('ls -la /var/www/html');
-echo "</pre>";
-
-echo "<h1>Permissions du répertoire</h1>";
-echo "<pre>";
-system('ls -la /var/www/');
-echo "</pre>";
-
-echo "<h1>Configuration Apache</h1>";
-echo "<pre>";
-system('cat /etc/apache2/sites-available/000-default.conf');
-echo "</pre>";
-
-echo "<h1>PHP Info</h1>";
-phpinfo();
-?>
+header("Location: index.html");
+exit();
